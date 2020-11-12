867254573775a726e4232cbe5058a417f686b4bc
Graham Herceg <g.a.herceg@gmail.com>
2020-11-11 18:17:06 -0800

    receive keyboard interrupts

---
+++ src/drivers/keyboard.h
@@ -0,0 +1,8 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include "../tools.h"
+
+void install_keyboard_driver();
+
+#endif
--- src/tb_itr/timer.c
+++ src/tb_itr/timer.c
@@ -16,12 +16,19 @@ static void timer_callback(registers_t regs) {
 void init_timer(uint32 frequency) {
     register_interrupt_handler(IRQ0, &timer_callback);
 
+    // The value we send to the PIT is the value to divide it's input clock
+    // (1193180 Hz) by, to get our required frequency. Important to note is
+    // that the divisor must be small enough to fit into 16-bits.
     uint32 divisor = 1193180 / frequency;
 
+    // 0x36 sets clock to repeating mode (refresh after divisor hits 0)
+    // it also says we want to set the divisor value
+    // TODO: not sure what 0x43 is for so look into that
     outb(0x43, 0x36);
 
+    // must be sent as 2 separate bytes, not one 16 bit value. 
     uint8 l = (uint8)(divisor & 0xFF);
-    uint8 h = (uint8)( (divisor>>8) & 0xFF );
+    uint8 h = (uint8)((divisor>>8) & 0xFF);
 
     outb(0x40, l);
     outb(0x40, h);
